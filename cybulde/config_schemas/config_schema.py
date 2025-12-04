f8c52f20d525f38ed1f5c3741c280b213ba362ee
torresv1 <torres_vladimir@hotmail.com>
2025-12-03 20:50:24 -0500

    Add comments to clarify DVC remote configuration in config_schema.py

--- cybulde/config_schemas/config_schema.py
+++ cybulde/config_schemas/config_schema.py
@@ -4,8 +4,8 @@ from pydantic.dataclasses import dataclass
 
 @dataclass
 class Config:
-    dvc_remote_name: str = "gcs-storage"
-    dvc_remote_url: str = "gs://cybulde1/data/raw"
+    dvc_remote_name: str = "gcs-storage"  # Name of the DVC remote
+    dvc_remote_url: str = "gs://cybulde1/data/raw"  # Google Cloud Storage bucket URL
     dvc_raw_data_folder: str = "data/raw"
 
 
